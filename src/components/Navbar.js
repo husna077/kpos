a4144c7cb9ca5a2aff0f5bf182d62d1d38effb1f
Mahdiya Husna <shaikhhusna077@gmail.com>
2025-04-29 17:56:42 +0530

    changes sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,10 @@ function Navbar({onToggleSidebar}){
    
     return(
         <div className="navbar">
-            
+            <div className="k-heading-sidebar">
             <h3 id="navbar-heading">Kaushalya</h3>
             <button className="menu-btn" onClick={onToggleSidebar}>☰</button>
+            </div>
             <div className="main-user">
                 <p id="main-user-text">Main User</p>
             </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './Sidebar.css';
+import logo from "../assets/logo.webp";
 
 function Sidebar({ visible }) {
 
     return (
         <div className="sidebar">
+            
             {visible && (
                 <div className="sidebar-content">
-                    <ul className="sidebar-content">
-                        <li classname="sidebar-list"><Link to="/dashboard">Dashboard</Link></li>
-                        <li classname="sidebar-list"><Link to="/reports">Reports</Link></li>
-                        <li classname="sidebar-list"><Link to="/supply">Supply</Link></li>
-                        <li classname="sidebar-list"><Link to="/products">Products</Link></li>
-                        <li classname="sidebar-list"><Link to="/users">Users</Link></li>
-                        <li classname="sidebar-list"><Link to="/settings">Settings</Link></li>
+                    <div >
+                        <img src={logo} className="k-logo"/>
+                        </div>
+                    <ul className="sidebar-content-list">
+                        <li><Link to="/dashboard">Dashboard</Link></li>
+                        <li><Link to="/reports">Reports</Link></li>
+                        <li><Link to="/supply">Supply</Link></li>
+                        <li><Link to="/products">Products</Link></li>
+                        <li><Link to="/users">Users</Link></li>
+                        <li><Link to="/settings">Settings</Link></li>
                     </ul>
                 </div>
             )}
