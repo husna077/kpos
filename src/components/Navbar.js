c71e907c6292a33b866f05a5bc9a55d8bddb87bb
Mahdiya Husna <shaikhhusna077@gmail.com>
2025-05-27 22:19:19 +0530

    final design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { FaChevronDown } from 'react-icons/fa'; // Font Awesome
-import { Link } from "react-router-dom";
+import {NavLink, Link } from "react-router-dom";
 import { VscAccount } from "react-icons/vsc";
 
 
@@ -18,19 +18,21 @@ function Navbar({onToggleSidebar}){
         }
         
     }
-   
+    const [openDropdown, setOpenDropdown] = useState(null);
+    
+      const handleDropdownToggle = (menuName) => {
+        setOpenDropdown(prev => (prev === menuName ? null : menuName));
+      };
     return(
         <div className="navbar">
             <div className="k-heading-sidebar">
             <h3 id="navbar-heading">Kaushalya</h3>
             <button className="menu-btn" onClick={onToggleSidebar}>☰</button>
             </div>
-            <div className="main-user" onClick={toggleDropdown}>
-                
-                
+            
+            <div className="main-user" onClick={toggleDropdown}>  
                 <span className="account-name"><VscAccount /> Main User< FaChevronDown /></span>
                 {dropDownOpen && 
-                
                     <ul className="dropdown-menu">
                         <li><Link to="/mainUser/setting" ><lable id="setting-id">Settings</lable></Link></li>
                         <li><button className="logout-btn" onClick={handleLogout}>Log Out</button></li>
